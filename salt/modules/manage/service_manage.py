9cfd9dbce220f3e17ddaf5063ea5e627b269e5fa
Gary Du <gudu9182@uni.sydney.edu.au>
2021-12-24 13:20:54 +0800

    修复开关服务大小写敏感问题

--- salt/modules/manage/service_manage.py
+++ salt/modules/manage/service_manage.py
@@ -1,4 +1,3 @@
-from typing import Union
 from wechaty import Room, Contact, Message
 from salt import on_command
 from salt.service import _loaded_service
@@ -10,7 +9,7 @@ async def enable_service(event: "Message", msg: str):
     conversation: "Room" = event.room()
     command = re.match("^(开启|启用) *(?P<command>.+)", msg).group("command")
     for sv_name, sv in _loaded_service.items():
-        if sv_name == command:
+        if sv_name.lower() == command.lower():
             await sv.enable_service(conversation)
             await conversation.say(f"已成功开启服务 - {sv_name}")
             sv.logger.info(f"服务{sv_name}已成功在{await conversation.topic()}开启")
@@ -23,7 +22,7 @@ async def enable_service(event: "Message", msg: str):
     conversation: "Room" = event.room()
     command = re.match("^(关闭|禁用) *(?P<command>.+)", msg).group("command")
     for sv_name, sv in _loaded_service.items():
-        if sv_name == command:
+        if sv_name.lower() == command.lower():
             await sv.disable_service(conversation)
             await conversation.say(f"已成功禁用服务 - {sv_name}")
             sv.logger.info(f"服务{sv_name}已成功在{await conversation.topic()}关闭")
